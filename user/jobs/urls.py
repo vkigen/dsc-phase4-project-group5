a9865cbec86b4045a1aa4cf63c87543d3baa714c
vkigen <victor.kigen1@student.moringaschool.com>
2025-01-27 12:23:14 +0300

    DJ V4

--- user/jobs/urls.py
+++ user/jobs/urls.py
@@ -6,4 +6,5 @@ app_name = "jobs"
 urlpatterns = [
     path("", views.home, name="home"),  # Form submission page
     path("results/", views.results, name="results"),  # Results display page
+    path('process_data/', views.process_data, name='process_data'),
 ]
